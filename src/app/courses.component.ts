cd6964dd353207ee15c29a7ad2c3ca73b97d5972
manrabs <muhammedrabiuumar@gmail.com>
2021-12-03 22:31:01 -0600

    added bootstrap and played with binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,8 +1,24 @@
-
+import { CoursesService } from './courses.service';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+        <button [style.backgroundColor]="isActive ? 'blue': 'black'"class="btn btn-primary" [class.active]="isActive">Save</button>
+        <h2>{{ title }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
 })
-export class CoursesComponent{}
+export class CoursesComponent{
+    title = "List of courses";
+    courses;
+    isActive = false;
+
+    constructor(service: CoursesService){
+        this.courses = service.getCourses();
+    }
+}
