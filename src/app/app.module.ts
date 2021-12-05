efcd0146ae1df5cf7e889e6c3d931245f41b6c60
manrabs <muhammedrabiuumar@gmail.com>
2021-12-04 22:35:40 -0600

    Custom Pipes (Pipe it up)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SummaryPipe } from './summary.pipe';
 import { CoursesService } from './courses.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -12,7 +13,8 @@ import { FormsModule } from '@angular/forms';
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
+        {{text | summary}}
+        
         <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
         <button [style.backgroundColor]="isActive ? 'blue': 'black'"class="btn btn-primary" [class.active]="isActive" (click)="onSave($event)">Save</button>
         <h2>{{ title }}</h2>
@@ -15,6 +17,10 @@ import { Component } from '@angular/core';
     `
 })
 export class CoursesComponent{
+    constructor(service: CoursesService){
+        this.courses = service.getCourses();
+    }
+
     title = "List of courses";
     courses;
     isActive = false;
@@ -28,8 +34,14 @@ export class CoursesComponent{
         console.log("<-- number of times button was clicked");
     }
 
-    constructor(service: CoursesService){
-        this.courses = service.getCourses();
-    }
+    text = `
+    Ayy, the blues is now kickin' and dinner is three Michelin
+    I don't eat red meat, but still got beef sizzlin' Know that I need discipline
+    I keep singin' for all these hoes, they keep listenin'
+    Niggas love to bro up with the boy and dap fists-es
+    But we are not equivalent, dawg
+    I been an only child, don't need siblings
+    And I'm past them like the times that he's livin' in, okay
+    `
 
 }
